f76880a917a4a5b5431fd82f8af46b23f7f59db4
Edson Dias - Trunfo <edson.designer2015@hotmail.com>
2022-04-22 11:49:54 -0300

    Creating the structure of components.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-
+import Header from  './components/Header';
+import Article from './components/Article';
 
 function App() {
   return (
-    <h1>Meu Blog</h1>
+
+    <>
+      <Header></Header>
+      <Article></Article>
+    </>
+    
+
   );
 }
 
---
+++ src/components/Header/index.js
@@ -0,0 +1,8 @@
+const Header = () => {
+
+    return (
+        <h1>Header.js</h1>
+    );
+}
+
+export default Header;
