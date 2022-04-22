d2c41a8a84f631e24689bf80e39911b8735811c7
Edson Dias - Trunfo <edson.designer2015@hotmail.com>
2022-04-22 13:40:07 -0300

    Styling the menu.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,33 @@
+import logosvg from "../../svg/logotipo.svg";
+
 const Header = () => {
 
+    const descLogo = "Essa é a logo do Blog.";
+
     return (
-        <h1>Header.js</h1>
+        <>
+            <header className="flex-space-beetween">
+
+                <div className="logo border">
+                    <img src={logosvg} alt={descLogo}/>
+                </div>
+                <div className="search">
+                    <input type="text" nome="search" className="input-search"/>
+                </div>
+                <ul className="menu">
+                    <li>
+                        <a href="#" className="nav-link">Categories</a>
+                    </li>
+                    <li>
+                        <a href="#" className="nav-link">About</a>
+                    </li>
+                    <li>
+                        <a href="#" className="nav-link">Contact</a>
+                    </li>
+                </ul>
+
+            </header>
+        </>
     );
 }
 
